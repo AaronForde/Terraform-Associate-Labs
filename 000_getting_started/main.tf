6bf57ad77cfcc5fc57d46c671ccc960c2405d449
Aaron Forde <aaronforde01@gmail.com>
2024-03-15 11:15:12 -0700

    Terraform Basics

--- 000_getting_started/main.tf
+++ 000_getting_started/main.tf
@@ -1,29 +1,65 @@
+# terraform {
+#   #backend "remote" {
+#   #  hostname = "app.terraform.io"
+#   #  organization = "ExamPro"
+
+#   #  workspaces {
+#   #    name = "getting-started"
+#   #  }
+#   #}
+#   cloud {
+#     hostname = "app.terraform.io"
+#     organization = "ExamPro"
+
+#     workspaces {
+#       name = "getting-started"
+#     }
+#   }
+
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = "~> 5.0"
+#     }
+#   }
+# }
+
+# locals {
+#   project_name = "Andrew"
+# }
+
 terraform {
-  #backend "remote" {
-  #  hostname = "app.terraform.io"
-  #  organization = "ExamPro"
-
-  #  workspaces {
-  #    name = "getting-started"
-  #  }
-  #}
-  cloud {
-    hostname = "app.terraform.io"
-    organization = "ExamPro"
-
-    workspaces {
-      name = "getting-started"
-    }
-  }
-
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "5.40.0"
     }
   }
 }
 
+provider "aws" {
+  region  = "ca-central-1"
+  profile = "default"
+}
+
+variable "instance_type" {
+  type = string
+}
+
 locals {
-  project_name = "Andrew"
+  project_name = "Aaron"
+  
+}
+
+resource "aws_instance" "my_server" {
+  ami           = "ami-0e817e890550ad158"
+  instance_type = var.instance_type
+
+  tags = {
+    Name = "myServer-${local.project_name}"
+  }
+}
+
+output "instance_ip_addr" {
+  value = aws_instance.my_server.public_ip
 }
